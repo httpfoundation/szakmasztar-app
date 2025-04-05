b82181d6f7e25a82d72ad2431610c50e57f27511
Oliver Mrakovics <oliver.mrakovics@gmail.com>
2025-04-05 13:53:56 +0200

    feat: vertical sector cards

--- src/components/skills/SectorCards.tsx
+++ src/components/skills/SectorCards.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import { Box, IconButton, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { Sector } from "@/app/szakmai-programok/page";
+import SectionContainer from "../layouts/SectionContainer";
 import SkillCategoryCard from "./SkillCategoryCard";
 
 interface SectorCardsProps {
@@ -11,88 +10,22 @@ interface SectorCardsProps {
 }
 
 const SectorCards = ({ sectors }: SectorCardsProps) => {
-  const sectorPairs = sectors.reduce<Sector[][]>((pairs, sector, index) => {
-    if (index % 2 === 0) {
-      pairs.push([sector, sectors[index + 1]].filter(Boolean));
-    }
-    return pairs;
-  }, []);
   return (
-    <Box sx={{ position: "relative" }}>
-      <IconButton
-        sx={{
-          position: "absolute",
-          left: 0,
-          top: "50%",
-          transform: "translateY(-50%)",
-          zIndex: 1,
-          display: { xs: "none", md: "flex" },
-          bgcolor: "white",
-          "&:hover": { bgcolor: "white" },
-        }}
-        onClick={() => {
-          const container = document.getElementById("scroll-container");
-          if (container) {
-            container.scrollBy({ left: -300, behavior: "smooth" });
-          }
-        }}
-      >
-        <ChevronLeftIcon />
-      </IconButton>
-
+    <SectionContainer sx={{ mt: -4, pb: 4 }}>
       <Stack
-        id="scroll-container"
         sx={{
           gap: 2,
-          display: "flex",
-          flexDirection: "row",
-          overflowX: "auto",
-          width: "80",
-          "&::-webkit-scrollbar": {
-            display: "none",
-          },
-          scrollbarWidth: "none",
-          msOverflowStyle: "none",
-          "& > *": {
-            flex: "0 0 calc(33.333% - 16px)",
-            minWidth: "calc(33.333% - 16px)",
-          },
+          display: "grid",
+          gridTemplateColumns: { xs: "repeat(2,1fr)", sm: "repeat(3,1fr)", md: "repeat(4,1fr)" },
         }}
       >
-        {sectorPairs.map((sectorPair) => (
-          <Stack direction="column" spacing={2} key={sectorPair[0].id}>
-            <SkillCategoryCard active={false} sector={sectorPair[0]} />
-            {sectorPair[1] ? (
-              <SkillCategoryCard active={false} sector={sectorPair[1]} />
-            ) : (
-              <Box sx={{ height: 73 }} />
-            )}
-          </Stack>
+        {sectors.map((sector) => (
+          <SkillCategoryCard key={sector.name} active={false} sector={sector} />
         ))}
       </Stack>
-
-      <IconButton
-        sx={{
-          position: "absolute",
-          right: 0,
-          top: "50%",
-          transform: "translateY(-50%)",
-          zIndex: 1,
-          display: { xs: "none", md: "flex" },
-          bgcolor: "white",
-          "&:hover": { bgcolor: "white" },
-        }}
-        onClick={() => {
-          const container = document.getElementById("scroll-container");
-          if (container) {
-            container.scrollBy({ left: 300, behavior: "smooth" });
-          }
-        }}
-      >
-        <ChevronRightIcon />
-      </IconButton>
-    </Box>
+    </SectionContainer>
   );
 };
 
 export default SectorCards;
+
