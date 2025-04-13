7ae3804e8e8cbebd3e68b31209aca5455c44e438
Zoltan Sisak <zoltan.sisak@gmail.com>
2025-04-13 16:33:32 +0200

    QR-code page update

--- src/app/qrkod/page.tsx
+++ src/app/qrkod/page.tsx
@@ -1,25 +1,37 @@
 import PageTitle from "@/components/common/PageTitle";
+import FormattedContent from "@/components/FormattedContent";
 import PageContainer from "@/components/layouts/PageContainer";
+import GradientTitle from "@/components/ui/GradientTitle";
 import QrCodeScanner from "./QrCodeScanner";
 
 export const dynamic = "force-static";
 
 const QrCodePage = async () => {
   return (
-    <PageContainer
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        height: "100%",
-        flexGrow: 1,
-      }}
-    >
-      <PageTitle sx={{ textAlign: "center" }}>QR kód beolvasása</PageTitle>
-      <QrCodeScanner />
-    </PageContainer>
+    <>
+      <GradientTitle>QR-kód beolvasása</GradientTitle>
+      <PageContainer
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+          flexGrow: 1,
+        }}
+      >
+        <FormattedContent variant="body1" sx={{ textAlign: "center", mb: 3 }}>
+          Olvasd be a szakmai program QR-kód-ját! Első alkalommal engedélyezned kell a kamerád
+          használatát az alkalmazás számára. Ha nem működik a telefonodon a QR-kód beolvasás, akkor
+          a nyitólapon be tudod írni a kielyezett számkódot is.
+        </FormattedContent>
+        <FormattedContent variant="body1" sx={{ textAlign: "center", mb: 3 }}>
+          A kód beolvasása után azonnal megjelennek a program részletei.
+        </FormattedContent>
+        <QrCodeScanner />
+      </PageContainer>
+    </>
   );
 };
 
