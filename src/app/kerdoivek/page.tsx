0f45b932147d975ceb5e9bd8504b7e51bff7318c
Botond Sisak <sisak.botond@gmail.com>
2025-04-13 14:19:48 +0200

    feat: kerdoivek formazas

--- src/app/kerdoivek/page.tsx
+++ src/app/kerdoivek/page.tsx
@@ -1,33 +1,35 @@
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import { Stack } from "@mui/material";
 import ImageButton from "@/components/common/ImageButton";
-import PageTitle from "@/components/common/PageTitle";
-import PageContainer from "@/components/layouts/PageContainer";
+import SectionContainer from "@/components/layouts/SectionContainer";
+import GradientTitle from "@/components/ui/GradientTitle";
 
 export const dynamic = "force-static";
 
 const KerdoivekPage = () => {
   return (
-    <PageContainer>
-      <PageTitle>Kérdőívek</PageTitle>
-      <Stack direction="column" spacing={2}>
-        <ImageButton
-          href="/kerdoivek/eredmenyorientacios"
-          icon={<QuestionMarkIcon />}
-          text="Eredményorientációs kérdőív"
-        />
-        <ImageButton
-          href="/kerdoivek/iparagi-erdeklodes"
-          icon={<QuestionMarkIcon />}
-          text="Iparági érdeklődési kérdőív"
-        />
-        <ImageButton
-          href="/kerdoivek/kompetencia"
-          icon={<QuestionMarkIcon />}
-          text="Kompetencia kérdőív"
-        />
-      </Stack>
-    </PageContainer>
+    <>
+      <GradientTitle>Kérdőívek</GradientTitle>
+      <SectionContainer sx={{ py: 4, px: 2 }}>
+        <Stack direction="column" spacing={2}>
+          <ImageButton
+            href="/kerdoivek/eredmenyorientacios"
+            icon={<QuestionMarkIcon />}
+            text="Eredményorientációs kérdőív"
+          />
+          <ImageButton
+            href="/kerdoivek/iparagi-erdeklodes"
+            icon={<QuestionMarkIcon />}
+            text="Iparági érdeklődési kérdőív"
+          />
+          <ImageButton
+            href="/kerdoivek/kompetencia"
+            icon={<QuestionMarkIcon />}
+            text="Kompetencia kérdőív"
+          />
+        </Stack>
+      </SectionContainer>
+    </>
   );
 };
 
