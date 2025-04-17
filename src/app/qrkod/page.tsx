b3ade5bfc818338329bb4524b3fb9454e48ae7e9
Oliver Mrakovics <oliver.mrakovics@gmail.com>
2025-04-17 10:15:48 +0200

    fix: QR code text size to always be seen on mobile

--- src/app/qrkod/page.tsx
+++ src/app/qrkod/page.tsx
@@ -18,19 +18,14 @@ const QrCodePage = async () => {
           justifyContent: "center",
           width: "100%",
           height: "100%",
-          flexGrow: 1,
-          pt: 0,
+          pt: 2,
           position: "relative",
         }}
       >
         <Starform />
         <FormattedContent variant="body1" sx={{ textAlign: "center", mb: 3 }}>
           Olvasd be a szakmai program QR-kód-ját! Első alkalommal engedélyezned kell a kamerád
-          használatát az alkalmazás számára. Ha nem működik a telefonodon a QR-kód beolvasás, akkor
-          a nyitólapon be tudod írni a kielyezett számkódot is.
-        </FormattedContent>
-        <FormattedContent variant="body1" sx={{ textAlign: "center", mb: 3 }}>
-          A kód beolvasása után azonnal megjelennek a program részletei.
+          használatát az alkalmazás számára.
         </FormattedContent>
         <QrCodeScanner />
       </PageContainer>
