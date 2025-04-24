93ce2e016374f267fe4daa3d88b9da799f169aeb
Zoltan Sisak <zoltan.sisak@gmail.com>
2025-04-24 19:29:08 +0200

    fix: update link button text for clarity

--- src/app/_components/OpeningHours.tsx
+++ src/app/_components/OpeningHours.tsx
@@ -37,7 +37,7 @@ const OpeningHours = () => {
           alignSelf: "center",
         }}
       >
-        Ütemterv megtekintése
+        Programnaptár megtekintése
       </LinkButton>
     </SectionContainer>
   );
